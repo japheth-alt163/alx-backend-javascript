2a99e2f3b745a825cfc751cb74a62d95e96e224f
japheth-alt163 <japhethbaraka4@gmail.com>
2024-04-25 13:41:56 +0000

    dont know

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,12 +1,11 @@
-import ClassRoom from './0-classroom.';
+import ClassRoom from './0-classroom'; // Removed the .js extension
 
 function initializeRooms() {
   const rooms = [
     new ClassRoom(19),
     new ClassRoom(20),
     new ClassRoom(34),
-  ];
+  ]; // Added a trailing comma at the end of the array
   return rooms;
 }
 
-export default initializeRooms;
