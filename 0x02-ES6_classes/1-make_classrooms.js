b707ea2374e4d93dacc9564634980d5052afe6ef
japheth-alt163 <japhethbaraka4@gmail.com>
2024-04-25 13:39:51 +0000

    fixed a comma on test case

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,10 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom.';
 
 function initializeRooms() {
   const rooms = [
     new ClassRoom(19),
     new ClassRoom(20),
-    new ClassRoom(34)
+    new ClassRoom(34),
   ];
   return rooms;
 }
