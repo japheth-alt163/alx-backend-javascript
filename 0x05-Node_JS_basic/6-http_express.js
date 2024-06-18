667c375e01e43b14d530f10db33db37a20e5d72d
japheth-alt163 <japhethbaraka4@gmail.com>
2024-06-18 16:57:31 +0000

    6-http_express.js

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,23 +1,14 @@
 const express = require('express');
 
-// Create an Express application
 const app = express();
+const port = 1245;
 
-// Define routes and middleware
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!\n');
+app.get('/', (request, response) => {
+  response.send('Hello Holberton School!');
 });
 
-// Handle 404 - Keep this as the last middleware
-app.use((req, res, next) => {
-  res.status(404).send('<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><title>Error</title></head><body><pre>Cannot GET ' + req.originalUrl + '</pre></body></html>');
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
 
-// Set up server to listen on port 1245
-const PORT = 1245;
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
-});
-
-// Export the app variable (Express application)
 module.exports = app;
